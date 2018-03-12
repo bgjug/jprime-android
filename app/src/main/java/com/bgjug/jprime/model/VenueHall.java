fa29c7bd43a161a624eda10c38ffebb5bc095f13
Kristian Shishoev <kshishoev@gmail.com>
2018-03-13 00:27:29 +0200

    Update VenueHall location and date

--- app/src/main/java/com/bgjug/jprime/model/VenueHall.java
+++ app/src/main/java/com/bgjug/jprime/model/VenueHall.java
@@ -2,16 +2,16 @@ package com.bgjug.jprime.model;
 
 public class VenueHall {
 
-	public static final String NAME = "Sofia Event Center";
-	public static final String JPRIME_DESCRIPTION = "jPrime is a conference with talks on Java, "
-			+ "various languages on the JVM, mobile, web and best practices."
-			+ "Its second edition will be held on 26th and 27th May 2016 in Sofia Event Center. "
-			+ "It's run by the Bulgarian Java User Group and backed by the biggest companies in the city. ";
-	public static final String GOOGLE_MAPS_URL_SEC = "https://www.google.bg/maps/place/bulevard+%22Cherni+vrah%22+100,+1407+Sofia/"
-			+ "@42.6578803,23.3124705,17z/data=!3m1!4b1!4m5!3m4!1s0x40aa845dd5e00623:0xa182f7bbf955f668!8m2!3d42.6578803!4d23.3146592?hl=en";
-	public static final String DATE = "26th and 27th May 2016";
-	public static final String LOCATION = "Paradise Center bulevard Cherni vrah 100";
-	public static final String TITLE = "Java Prime Time in Sofia is held on 26th and 27th May 2016 in Sofia Event Center";
+	public static final String NAME = "Sofia Tech Park";
+	public static final String JPRIME_DESCRIPTION = "jPrime is a conference with talks on Java, various languages on the JVM, mobile, web and best practices. " +
+			"Its forth edition will be held on 29th and 30th May 2018 in Sofia Tech Park. It's run by the Bulgarian Java User Group and backed by the biggest companies in the city.";
+
+	public static final String GOOGLE_MAPS_URL_SEC = "https://www.google.bg/maps/place/Sofia+Tech+Park/@42.6699844,23.359686,15z/" +
+			"data=!4m13!1m7!3m6!1s0x40aa8682cb317bf5:0x400a01269bf5e60!2sSofia!3b1!8m2!3d42.6977082!4d23.3218675!3m4!1s0x0:0xcbc66ddbb54d08b3!8m2!3d42.6669177!4d23.3733702";
+
+	public static final String DATE = "29th and 30th May 2018";
+	public static final String LOCATION = "Tsarigradsko Shosse 115B, Sofia, Bulgaria";
+	public static final String TITLE = "Java Prime Time in Sofia is held on 29th and 30th May 2018 in Sofia Tech Park";
 	public static final String EMAIL = "conference@jprime.io";
 
 }
